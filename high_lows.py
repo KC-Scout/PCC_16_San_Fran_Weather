a3d70b3dbf462184911fc9f84596a24dcad36a59
Connor Foster <foster.clee@gmail.com>
2019-04-29 20:58:28 -0500

    Revert "use the program with data from death valley. There is one date missing that causes an error to occur when you run the program. Develop a try-except-else block to deal with the error"
    This reverts commit 148358be4db2652a9321c30d73deb5a4823c5ac3.

--- high_lows.py
+++ high_lows.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from matplotlib import pyplot as plt
 
 # Get high temperatures from file
-filename = 'death_valley_2014.csv'
+filename = 'sitka_weather_2014.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
@@ -11,16 +11,12 @@ with open(filename) as f:
     # Get current dates low and high temperatures from file
     dates, highs, lows = [], [], []
     for row in reader:
-        try:
-            current_date = datetime.strptime(row[0], '%Y-%m-%d')
-            high = int(row[1])
-            low = int(row[3])
-        except ValueError:
-            print(current_date, 'missing data')
-        else:
-            dates.append(current_date)
-            highs.append(high)
-            lows.append(low)
+        current_date = datetime.strptime(row[0], '%Y-%m-%d')
+        dates.append(current_date)
+        high = int(row[1])
+        highs.append(high)
+        low = int(row[3])
+        lows.append(low)
           
   
 # Plot data
@@ -30,8 +26,7 @@ plt.plot(dates, lows, c = 'blue')
 plt.fill_between(dates, highs, lows, facecolor = 'blue', alpha=.1)
 
 # Format plot
-title = "Daily high and low temperatures - 2014 \nDeath Valley, CA"
-plt.title(title, fontsize = 20)
+plt.title("Daily High & Low temperatures - 2014", fontsize = 24)
 plt.xlabel('', fontsize = 16)
 fig.autofmt_xdate()
 plt.ylabel('Temperature(F)', fontsize = 16)
